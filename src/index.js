505b51fa132e3b887491eea823fba7f55db1c283
Neo <petrenko-m-p@mail.ru>
2021-10-12 02:10:51 +0700

    q

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function ImageLink(props)
+{
+  return (
+    <a href={props.link}>
+      <img src = {props.src}></img>
+    </a>);
+}
+
+function LinksRow(props)
+{
+  var items = [
+    {src: './in.png', link:'https://www.instagram.com/'}, 
+    {src: './fb.png', link:'https://www.facebook.com'},
+  ];
+
+  return items.map(item => (<ImageLink src={item.src} link={item.link}/>));
+}
+
+function CenterAll()
+{
+  return (
+  <div style= {{display: 'flex', alignItems: 'center', justifyContent: 'center',}}>
+    <LinksRow/>
+  </div>)
+}
+
+ReactDOM.render(<CenterAll/>, document.getElementById('root'));
