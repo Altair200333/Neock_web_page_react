11326c92c0d546b1f36bed8581026c998ef69d4f
Neo <petrenko-m-p@mail.ru>
2021-10-13 00:27:31 +0700

    hower anim

--- src/index.js
+++ src/index.js
@@ -5,10 +5,17 @@ import './App.css';
 
 function ImageLink(props)
 {
+  const [hower, setHower] = useState(0);
+
   return (
-    <a href={props.link}>
-      <img src = {props.src}></img>
-    </a>);
+    <div  className="imageStack">
+      <a href={props.link}>
+        <img className="backImage" src='./back.png' style={{height:"100px"}}/>
+
+        <img className="linkImage" src = {props.src} style={{height:hower==0?"100px":"80px"}}
+            onMouseEnter ={() => {setHower(1)}} onMouseLeave={()=>{setHower(0)}}></img>
+      </a>
+    </div>);
 }
 
 function LinksRow(props)
