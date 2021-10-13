9e44540092ec4fbf34bde86b98c899f302a3f4f3
Neo <petrenko-m-p@mail.ru>
2021-10-14 02:02:07 +0700

    da

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ import './App.css';
 function ImageLink(props)
 {
   const [hower, setHower] = useState(0);
-  var baseW = "70px";
-  var scaledW = "50px";
+  var baseW = "60px";
+  var scaledW = "40px";
   return (
     <div  className="imageStack">
       <a href={props.link} onMouseEnter ={() => {setHower(1)}} onMouseLeave={()=>{setHower(0)}}>
@@ -29,12 +29,17 @@ function LinksRow(props)
   ];
 
   return (
-    <table align="center">
+    <table align="center" cellspacing="2" cellpadding="2" >
         <tr>
+          <td style={{color: "#575959"}}><h2>Altair 200333 &nbsp; </h2></td>
           {items.map(item => (<td><ImageLink src={item.src} link={item.link}/></td>))}
         </tr>
     </table>
     );
+}
+function PostsLinks(props)
+{
+  return (<h1>Kurwa</h1>);
 }
 
 function UI()
@@ -43,6 +48,7 @@ function UI()
   return (
     <div>
       <LinksRow/>
+      <PostsLinks/>
       <p>Text {count}</p>
       <p className='color' onMouseEnter ={() => {setCount(count + 1)}} onMouseLeave={()=>{setCount(count - 1)}} 
                     style={{color: count==1?"red":"gray"}}>
