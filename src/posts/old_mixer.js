edaad039802ea7c76837e9a08cb10d8e659b3552
Neo <petrenko-m-p@mail.ru>
2021-10-15 18:14:52 +0700

    posts

--- src/posts/old_mixer.js
+++ src/posts/old_mixer.js
@@ -1,8 +1,23 @@
 import React, { useState } from 'react';
 import '../App.css';
+import './styles.css';
+import styles from 'react-responsive-carousel/lib/styles/carousel.min.css'
+var Carousel = require('react-responsive-carousel').Carousel;
 
 function OldMixer()
 {
-    return (<h1>PAGE3</h1>)
+    return (
+        <div style={{width:'600px'}}>
+            <h1 className='title'>Simple 3D engine</h1>
+            <div className='postText'>
+                Custom simple 3D engine. All internal math magic is made from scratch in a way that there are no matricies (like in normal graphic API's).
+                <p/>
+                Initially was written as project for C classes at university but it was such a mess that later on I rewrote it to C++ and fixed some bugs.
+                <p/>
+                <img src = 'post_imgs/om1.jpg' className='exampleImg'/>
+                <div style={{height:'100px'}}/>
+            </div>   
+            </div>
+        );
 }
 export default OldMixer;
--- src/posts/voxel_ray.js
+++ src/posts/voxel_ray.js
@@ -11,14 +11,14 @@ function VoxelRay()
         <h1 className='title'>Realtime voxel raytracer</h1>
         <div className='postText'>
             Real-time voxel ray tracing engine. Supports 3 types of materials such as: glass,
-             mirror and diffuse colored block. Models can be imported from MagicaVoxel in .ply format
+            mirror and diffuse colored block. Models can be imported from MagicaVoxel in .ply format
         </div>
         <p/>
         <img src = 'post_imgs/v1.jpg' className='exampleImg'></img>
         <p/>
         <div className='postText'>
-        All tracing computations are performed on GPU in fragment shader.
-        Scene data is loaded via Buffer objects as 1D array.
+            All tracing computations are performed on GPU in fragment shader.
+            Scene data is loaded via Buffer objects as 1D array.
         <p/>
         <Carousel showArrows={true} showThumbs={false}>
                 <div>
@@ -29,7 +29,7 @@ function VoxelRay()
                 </div>
         </Carousel>
         <p/>
-        The main advantage of voxel raytracing is ability to traverse array without checking extra cells for collisions in the way described in this paper:<br/>
+            The main advantage of voxel raytracing is ability to traverse array without checking extra cells for collisions in the way described in this paper:<br/>
         <a href='https://pdfs.semanticscholar.org/7620/a26cf2ffc6a4d634c7cde816d2f716904d26.pdf'> A Fast Voxel Traversal Algorithm</a>
         </div>
 
