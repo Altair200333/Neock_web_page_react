f4be2a05cf1979d9c4996194e01b6e4424a19fb7
Neo <petrenko-m-p@mail.ru>
2021-10-18 01:49:32 +0700

    tag filter

--- src/main.js
+++ src/main.js
@@ -4,49 +4,59 @@ import {
     Link
   } from "react-router-dom";
 import pages from './pages'
+import PostLink from './post_short';
 
-function Tag(props)
+
+function TagFilter(props)
 {
-  const [hower, setHower] = useState(0);
-
   return (
-    <button className='tagBtn' style={{color:'white', backgroundColor:'#d14fff', padding: hower==0?'4px':'7px', borderRadius: '5px', fontWeight: 'bold'}}
-      onMouseEnter ={() => {setHower(1)}} onMouseLeave={()=>{setHower(0)}} onClick={() => {props.setFiler(props.tag)}}>
-      {props.tag}
-    </button>)
+    <table>
+      <tbody>
+        <tr>
+          <td>
+            <button style={{padding: '4px', borderRadius: '5px', fontWeight: 'bold'}} onClick={() => {props.setFilter('');}}>
+              Reset
+            </button>
+          </td>
+          <td>
+            <div style={{color:'white', backgroundColor:'#d14fff', padding: '4px', borderRadius: '5px', fontWeight: 'bold'}}>
+              {props.filter == '' ?  '...' : props.filter}
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  )
 }
-
-function PostLink(props)
+function PostsHead(props)
 {
-  const [hower, setHower] = useState(0);
-
-  return (  
-      <div onMouseEnter ={() => {setHower(1)}} onMouseLeave={()=>{setHower(0)}}>
-        <div style={{height:"10px", width:'500px'}}>
-          <Link to = {props.info.link} className="scaleText" style={{fontSize: 20 + hower * 5, color:'#9b00de', fontWeight:'bold'}}>{props.info.text}</Link>
-        </div>
-        
-        <p/>
-
-        <table cellPadding="2">
-          <tbody>
-            <tr style={{height: '40px'}}>
-              {props.info.tags.map(item => (<td key={item}><Tag tag={item} setFiler={props.setFiler}/></td>))}
-            </tr>
-          </tbody>
-        </table>
-
-        <div className='description'>{props.info.short}</div>
-        <p/>
-      </div>);
+  return (
+    <table>
+      <tbody>
+        <tr>
+          <td>
+            <h1 style={{ fontFamily: 'Courier New' }}>Posts</h1>
+          </td>
+          <td style={{width:'30px'}}/>
+          {
+            props.filter != ''?
+            <td>
+            <TagFilter setFilter={props.setFilter} filter={props.filter}/>
+            </td>:''
+          }
+        </tr>
+      </tbody>
+    </table>
+  );
 }
-
 function PostsLinks(props)
 {
   const [filter, setFilter] = useState('');
   return (
     <div>
-    <h1 style={{ fontFamily: 'Courier New' }}>Posts</h1>
+    <PostsHead setFilter = {setFilter} filter = {filter}/>
+    <br/>
+
     <table cellPadding="2" >
       <tbody>
           {pages.filter(page => filter == '' || page.tags.includes(filter)).map(item => (
---
+++ src/post_short.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import './App.css';
+import {
+    Link
+  } from "react-router-dom";
+import pages from './pages'
+
+
+
+function Tag(props)
+{
+  const [hower, setHower] = useState(0);
+
+  return (
+    <button className='tagBtn' style={{color:'white', backgroundColor:'#d14fff', padding: hower==0?'4px':'7px', borderRadius: '5px', fontWeight: 'bold'}}
+      onMouseEnter ={() => {setHower(1)}} onMouseLeave={()=>{setHower(0)}} onClick={() => {props.setFiler(props.tag)}}>
+      {props.tag}
+    </button>)
+}
+
+function PostLink(props)
+{
+  const [hower, setHower] = useState(0);
+
+  return (  
+      <div onMouseEnter ={() => {setHower(1)}} onMouseLeave={()=>{setHower(0)}}>
+        <div style={{height:"10px", width:'500px'}}>
+          <Link to = {props.info.link} className="scaleText" style={{fontSize: 20 + hower * 5, color:'#9b00de', fontWeight:'bold'}}>{props.info.text}</Link>
+        </div>
+        
+        <p/>
+
+        <table cellPadding="2">
+          <tbody>
+            <tr style={{height: '40px'}}>
+              {props.info.tags.map(item => (<td key={item}><Tag tag={item} setFiler={props.setFiler}/></td>))}
+            </tr>
+          </tbody>
+        </table>
+
+        <div className='description'>{props.info.short}</div>
+        <p/>
+      </div>);
+}
+
+export default PostLink;
